a96fd1ba2502e76626591914f7f8231e0d77f71b
Christian Rodriguez <cristian010193@hotmail.com>
2018-01-16 22:15:10 -0500

    Finalizacion Curso Efectos CSS
    Curso avanzado de HTML5, CSS y Javascript

---
+++ CSS-Efectos/animaciones/filtros/funciones.js
@@ -0,0 +1,10 @@
+(function() {
+  mixitup(".container", {
+    "animation": {
+      "duration": 250,
+      "nudge": true,
+      "reverseOut": true,
+      "effects": "fade rotateX(113deg) rotateZ(180deg)"
+    }
+  });
+})();
